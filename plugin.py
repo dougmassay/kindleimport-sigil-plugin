b2ee5296235d08bfe50d0df53bc4895020590120
Doug Massay <dougmassay@users.noreply.github.com>
2017-04-09 16:33:10 -0400

    Add workaround for Sierra's temporary PyQt5 woes

--- plugin.py
+++ plugin.py
@@ -63,12 +63,18 @@ def run(bk):
     global prefs
     global GUI
 
-    if bk.launcher_version() >= 20170115:
+    prefs = bk.getPrefs()
+
+    if 'force_tkinter_gui' not in prefs:
+        prefs['force_tkinter_gui'] = False
+        # immediately generate a json prefs file so the
+        # force_tkinter_gui setting can be edited if need be.
+        bk.savePrefs(prefs)
+
+    if bk.launcher_version() >= 20170115 and not prefs['force_tkinter_gui']:
         GUI = 'pyqt'
     else:
         GUI = 'tkinter'
-
-    prefs = bk.getPrefs()
 
     # set default preference values
     if 'use_file_path' not in prefs:
